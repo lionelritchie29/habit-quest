c8e3aefff33ac0dc20b6d7d585c4669c3c81875c
Lionel Ritchie <lionelrtchieee@gmail.com>
2021-12-28 09:43:39 +0700

    Initial commit

---
+++ app/src/main/java/com/mobile_prog/habit_quest/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mobile_prog.habit_quest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
