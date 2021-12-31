d15de7e486f98855f58b8faf73f6a77b8e425ae3
Mcooldown <vincenthadinata30@gmail.com>
2021-12-31 15:58:48 +0700

    fix model + adapter

--- app/src/main/java/models/Quest.java
+++ app/src/main/java/models/Quest.java
@@ -2,14 +2,14 @@ package models;
 
 public class Quest {
     public int id;
-    public int typeId;
+    public QuestType type;
     public String name;
     public String description;
     public String tips;
 
-    public Quest(int id, int typeId, String name, String description, String tips) {
+    public Quest(int id, QuestType questType, String name, String description, String tips) {
         this.id = id;
-        this.typeId = typeId;
+        this.type = questType;
         this.name = name;
         this.description = description;
         this.tips = tips;
--- app/src/main/java/models/UserQuest.java
+++ app/src/main/java/models/UserQuest.java
@@ -3,14 +3,14 @@ package models;
 public class UserQuest {
 
     public int id;
-    public int userId;
-    public int questTypeId;
-    public int[] questDoneIds;
+    public UserQuestType userQuestType;
+    public Quest quest;
+    public boolean isDone;
 
-    public UserQuest(int id, int userId, int questTypeId, int[] questDoneIds) {
+    public UserQuest(int id, UserQuestType userQuestType, Quest quest, boolean isDone) {
         this.id = id;
-        this.userId = userId;
-        this.questTypeId = questTypeId;
-        this.questDoneIds = questDoneIds;
+        this.userQuestType = userQuestType;
+        this.quest = quest;
+        this.isDone = isDone;
     }
 }
