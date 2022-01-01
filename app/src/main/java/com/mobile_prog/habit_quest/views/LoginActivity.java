83e15c7c377290c8d8a17805d7df2470b8af30db
Lionel Ritchie <lionelrtchieee@gmail.com>
2022-01-01 09:58:47 +0700

    hide action bar in login activity

--- app/src/main/java/com/mobile_prog/habit_quest/views/LoginActivity.java
+++ app/src/main/java/com/mobile_prog/habit_quest/views/LoginActivity.java
@@ -39,6 +39,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        getSupportActionBar().hide();
         disableNightMode();
         initializeHuaweiSingleSignOn();
     }
